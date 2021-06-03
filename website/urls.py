14ced71d6af5703cba71388ed4fef0ec8ce34421
paulopinto-21906966 <a21906966@alunos.ulht.pt>
2021-06-03 19:47:14 +0100

    +tarefas

--- website/urls.py
+++ website/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('outro/<str:number>', views.outro_page_view, name='outro'),
     path('about', views.about_page_view, name='about'),
     path('admin', views.admin_tarefa_view, name='admin'),
-    path('tarefas/', views.tarefas_tarefa_view, name='tarefas')
+    path('tarefas/', views.tarefas_tarefa_view, name='tarefas'),
+    path('nova/', views.nova_tarefa_view, name='nova'),
+    path('edita/<int:tarefa_id>', views.edita_tarefa_view, name='edita'),
+    path('apaga/<int:tarefa_id>', views.apaga_tarefa_view, name='apaga'),
 ]
